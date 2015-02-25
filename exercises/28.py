d894e39e0280aaa45cef914f2202e978797b26fb
Krzysztof Szyda <szyda.krzysztof@gmail.com>
2015-02-25 21:47:23 +0100

    Update and rename 2 to 28.py

---
+++ exercises/28.py
@@ -0,0 +1,10 @@
+'''
+Write a function find_longest_word()
+that takes a list of words and returns
+the length of the longest one.
+Use only higher order functions.
+'''
+
+
+def find_longest_word(lst):
+    return len(max(lst, key=len))
