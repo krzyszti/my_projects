0ba81b419fd557be1fe19beb1e013ba10b981f65
Krzysztof Szyda <szyda.krzysztof@gmail.com>
2015-02-25 20:32:01 +0100

    Update and rename exercises to exercises/16.py

---
+++ exercises/16.py
@@ -0,0 +1,14 @@
+'''
+Write a function filter_long_words()
+that takes a list of words and an
+integer n and returns the list of words
+that are longer than n.
+'''
+
+
+def filter_long_words(words, n):
+    result = []
+    for word in words:
+        if len(word)>n:
+            result.append(word)
+    return result
