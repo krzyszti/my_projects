c52a1f58e94f3847b9ef1d9889fae394de9a4852
Krzysztof Szyda <szyda.krzysztof@gmail.com>
2015-10-20 15:32:45 +0200

    Revert "move constant to different new"
    This reverts commit e5fa280dd298eba714ee58555a2a7530b2a24f3d.

--- Ruby/Tests/endomondo/features/step_definitions/endomondo_steps.rb
+++ Ruby/Tests/endomondo/features/step_definitions/endomondo_steps.rb
@@ -1,3 +1,9 @@
+USERS = {
+  valid: { email: 'krzysztof@yopmail.com', password: 'test', name: 'Krzysztof', gender: 'Male' },
+  invalid: { email: '1234', password: '1234', name: 'Test', gender: 'Female' }
+}
+
+
 Given(/^the app is launched$/) do
   
   @login_screen = page(LoginScreen).await
--- Ruby/Tests/endomondo/features/support/users.rb
+++
@@ -1,4 +0,0 @@
-USERS = {
-  valid: { email: 'krzysztof@yopmail.com', password: 'test', name: 'Krzysztof', gender: 'Male' },
-  invalid: { email: '1234', password: '1234', name: 'Test', gender: 'Female' }
-}
