33944ad2ef6f4c85b8c147054f523fc7c3173581
Krzysztof Szyda <krzysztof.szyda@intive.com>
2017-07-11 18:50:21 +0200

    excercises fixed

--- Python/Exercises/zadanie1.py
+++ Python/Exercises/zadanie1.py
@@ -1,33 +1,24 @@
-'''
+"""
 przyjmuje 2 stringi: "79-900" i "80-155" i zwraca listę kodów pomiędzy nimi
-'''
-def code(string_1, string_2):
+"""
+
+
+def de_code(s):
+    return int(''.join(s.split('-')))
+
+
+def code(i):
+    l = str(i)
+    return "{}-{}".format(l[:2], l[2:])
+
+
+def code_list(c_1, c_2):
+    code_1 = de_code(c_1)
+    code_2 = de_code(c_2)
     result = []
-    input_values = [string_1.split('-'), string_2.split('-')]
-    for i, value in enumerate(input_values):
-        input_values[i]=[int(value[0]),int(value[1])]
-    input_values[1][1]-=1
-    while input_values[0]!=input_values[1]:
-        if input_values[0][1]==999:
-            input_values[0][1]=0
-            input_values[0][0]+=1
-        else:
-            input_values[0][1]+=1
-        tmp = ''
-        if input_values[0][0]<10:
-            tmp+='0'+str(input_values[0][0])
-        else:
-            tmp+=str(input_values[0][0])
-        tmp+='-'
-        if input_values[0][1]<10:
-            tmp+='00'+str(input_values[0][1])
-        elif input_values[0][1]<10:
-            tmp+='0'+str(input_values[0][1])
-        else:
-            tmp+=str(input_values[0][1])
-        result.append(tmp)
+    for i in range(code_1 + 1, code_2):
+        result.append(code(i))
     return result
 
 
-def code(code_1, code_2):
-    result = []
+print(code_list("79-900", "80-155"))
--- Python/Exercises/zadanie3.py
+++ Python/Exercises/zadanie3.py
@@ -1,7 +1,17 @@
 """
 NALEŻY WYGENEROWAĆ LISTĘ LICZB OD 2 DO 5.5 ZE SKOKIEM CO 0.5, DANE WYNIKOWE MUSZĄ BYĆ TYPU DECIMAL.
 """
+import sys
 
 
-def numbers():
-    return list(0.5 * n for n in range(4, 12))
+def generate_list(frm, to, jump):
+    return [jump * n for n in range(int(frm * 2), int(to * 2) + 1)]
+
+
+def main():
+    numbers_list = generate_list(2, 5.5, 0.5)
+    print(numbers_list)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
